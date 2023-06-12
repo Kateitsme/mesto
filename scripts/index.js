8e49caa5c2af84572a5746d3eaf63a03e06c193c
Kateitsme <katyaefremova02@yandex.ru>
2023-06-12 20:46:40 +0300

    try 6/1

--- scripts/index.js
+++ scripts/index.js
@@ -9,10 +9,10 @@ const addBtn = document.querySelector('.profile__add-button');
 
 // profile edit popup
 const popupProfileClose = popupProfile.querySelector('.popup__close-btn');
-const popupProfileSave = popupProfile.querySelector('.popup__submit-btn');
+const popupProfileSave = popupProfile.querySelector('.form__submit-btn');
 const popupProfileForm = popupProfile.querySelector('.popup__form');
-const popupProfileName = popupProfile.querySelector('.popup__input_type_name');
-const popupProfileJob = popupProfile.querySelector('.popup__input_type_job');
+const popupProfileName = popupProfile.querySelector('.form__input_type_name');
+const popupProfileJob = popupProfile.querySelector('.form__input_type_job');
 
 //profile on page
 const profile = document.querySelector('.profile');
@@ -21,10 +21,10 @@ const profileJob = profile.querySelector('.profile__job');
 
 //new element popup
 const popupNewElementClose = popupNewElement.querySelector('.popup__close-btn');
-const popupNewElementSave = popupNewElement.querySelector('.popup__submit-btn');
+const popupNewElementSave = popupNewElement.querySelector('.form__submit-btn');
 const popupNewElementForm = popupNewElement.querySelector('.popup__form');
-const popupNewElementName = popupNewElement.querySelector('.popup__input_type_name');
-const popupNewElementLink = popupNewElement.querySelector('.popup__input_type_link');
+const popupNewElementName = popupNewElement.querySelector('.form__input_type_place');
+const popupNewElementLink = popupNewElement.querySelector('.form__input_type_link');
 
 //image popup
 const popupImageClose = popupImage.querySelector('.popup__close-btn');
@@ -34,18 +34,73 @@ const popupImageImg = popupImage.querySelector('.popup__img');
 const elementTemplate = document.querySelector('#template');
 const elements = document.querySelector('.elements');//parent
 
+/**FUNCTIONS */
+//open popup
 const openPopup = item => {
   item.classList.add('popup_opened');
+  document.addEventListener('keydown', closePopupEsc);
 };
 
+function openProfilePopup() {
+  popupProfileName.value = profileName.textContent;
+  popupProfileJob.value = profileJob.textContent;
+  resetPopupEditProfile(config, [popupProfileName, popupProfileJob], popupProfileSave);
+  openPopup(popupProfile);
+}
+
+function openNewElementPopup() {
+  popupNewElementForm.reset();
+  resetPopupNewElement(config, [popupNewElementName, popupNewElementLink], popupNewElementSave);
+  openPopup(popupNewElement);
+}
+
+function openImagePopup(title, link) {
+  imagePopupTitle.textContent = title;
+  imagePopupImage.setAttribute('src', link);
+  imagePopupImage.setAttribute('alt', title);
+  openPopup(imagePopup);
+}
+
+//close popup
 const closePopup = item => {
   item.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEsc);
+};
+//close by esc
+const closePopupEsc = evt => {
+  evt.preventDefault();
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  };
+};
+//close by click
+const closePopupClick = evt => {
+  const openedPopup = document.querySelector('.popup_opened');
+  const closeBtn = openedPopup.querySelector('.popup__close-btn');
+  if (evt.target.contains(closeBtn)) {
+    closePopup(openedPopup);
+  };
 };
 
+const changeUserData = evt => {
+  evt.preventDefault();
+  profileName.textContent = popupProfileName.value;
+  profileJob.textContent = popupProfileJob.value;
+  closePopup(popupProfile);
+}
+
+const addNewElement = evt => {
+  evt.preventDefault();
+  renderElement(popupNewElementName.value, popupNewElementLink.value);
+  closePopup(popupNewElement);
+};
+
+//remove element
 const removeElement = event => {
   event.target.closest('.element').remove();
 };
-
+//press like btn
 const toggleLikeBtn = event => {
   event.target.classList.toggle('element__like-btn_active');
 };
@@ -70,7 +125,7 @@ const createElement = ({ name, link }) => {
   });
 
   element.querySelector('.element__like-btn').addEventListener('click', toggleLikeBtn);
-  element.querySelector('.element__del-btn').addEventListener('click', event => { removeElement(event) });
+  element.querySelector('.element__del-btn').addEventListener('click', removeElement);
 
   return element;
 };
@@ -79,34 +134,28 @@ const renderElement = (name, link) => {
   elements.prepend(createElement({ name, link }));
 };
 
-const addNewElement = evt => {
-  evt.preventDefault();
-  renderElement(popupNewElementName.value, popupNewElementLink.value);
-  closePopup(popupNewElement);
-  popupNewElementForm.reset();
-};
-//render default cards
-initialCards.forEach(item => {
-  renderElement(item.name, item.link);
-});
-//change profile
-popupProfileForm.addEventListener('submit', evt => {
-  evt.preventDefault();
-  profileName.textContent = popupProfileName.value;
-  profileJob.textContent = popupProfileJob.value;
-  closePopup(popupProfile);
-});
-//open popups
-addBtn.addEventListener('click', () => { openPopup(popupNewElement); });
-editBtn.addEventListener('click', () => {
-  popupProfileName.value = profileName.textContent;
-  popupProfileJob.value = profileJob.textContent;
-  openPopup(popupProfile);
-});
+//render default elements
+initialCards.forEach(item => renderElement(item.name, item.link));
 
-//add new element
-popupNewElementForm.addEventListener('submit', evt => { addNewElement(evt); });
-//close popups
-popupImageClose.addEventListener('click', () => { closePopup(popupImage) });
-popupProfileClose.addEventListener('click', () => { closePopup(popupProfile) });
-popupNewElementClose.addEventListener('click', () => { closePopup(popupNewElement); });
+
+function setEventListenersOnPopups() {
+  editBtn.addEventListener('click', openProfilePopup);
+  addBtn.addEventListener('click', openNewElementPopup);
+
+  popupProfileClose.addEventListener('click', closePopup(popupProfile));
+  popupNewElementClose.addEventListener('click', closePopup(popupNewElement));
+  popupImageClose.addEventListener('click', closePopup(popupImage));
+
+  popupProfileForm.addEventListener('submit', changeUserData);
+  popupNewElementForm.addEventListener('submit', addNewElement);
+
+  popupProfile.addEventListener('click', closePopupClick);
+  popupNewElement.addEventListener('click', closePopupClick);
+  popupImage.addEventListener('click', closePopupClick);
+
+  popupProfile.addEventListener('keydown', closePopupEsc);
+  popupNewElement.addEventListener('keydown', closePopupEsc);
+  popupImage.addEventListener('keydown', closePopupEsc);
+}
+
+setEventListenersOnPopups();
---
+++ scripts/validate.js
@@ -0,0 +1,85 @@
+const config = {
+  formSelector: '.popup__form',
+  inputSelector: '.form__input',
+  submitButtonSelector: '.form__submit-btn',
+  inputErrorClass: 'form__input_type_error',
+  errorClass: 'form__input-error_active'
+};
+const showInputError = (config, inputElement, errorElement, errorMessage) => {
+  inputElement.classList.add(config.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(config.errorClass);
+};
+
+const hideInputError = (config, inputElement, errorElement) => {
+  inputElement.classList.remove(config.inputErrorClass);
+  errorElement.textContent = '';
+  errorElement.classList.remove(config.errorClass);
+};
+
+const checkInputValidity = (config, inputElement, errorElement) => {
+  if (inputElement.validity.valid) {
+    hideInputError(config, inputElement, errorElement);
+  } else {
+    showInputError(config, inputElement, errorElement, inputElement.validationMessage);
+  }
+};
+
+const setEventListeners = (config, formElement, submitButtonElement) => {
+  const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
+
+  inputList.forEach((inputElement) => {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    submitButtonElement.setAttribute('disabled', true);
+
+    inputElement.addEventListener('input', () => {
+      checkInputValidity(config, inputElement, errorElement);
+      toggleButtonState(config, inputList, submitButtonElement);
+    });
+  });
+};
+
+const enableValidation = (config) => {
+  const formList = Array.from(document.querySelectorAll(config.formSelector));
+
+  formList.forEach((formElement) => {
+    const submitButtonElement = formElement.querySelector(config.submitButtonSelector);
+
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(config, formElement, submitButtonElement);
+  });
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+// В зависимости от валидации делает кнопку отправки формы активной или неактивной
+const toggleButtonState = (config, inputList, submitButtonElement) => {
+  if (hasInvalidInput(inputList)) {
+    submitButtonElement.setAttribute('disabled', true);
+  } else {
+    submitButtonElement.removeAttribute('disabled');
+  }
+};
+
+function resetPopupNewElement(config, inputList, submitButton) {
+  submitButton.setAttribute('disabled', true);
+  inputList.forEach((input) => {
+    hideInputError(config, input, input.nextElementSibling);
+  });
+}
+
+function resetPopupEditProfile(config, inputList, submitButton) {
+  submitButton.removeAttribute('disabled');
+  inputList.forEach((input) => {
+    hideInputError(config, input, input.nextElementSibling);
+  });
+}
+
+enableValidation(config);
